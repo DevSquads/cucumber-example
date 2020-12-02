114c7755a2d226257c081b7c82d181ae9dc84f73
A.Yasser <ahmed.yasser.asu@gmail.com>
2020-12-02 15:32:09 +0200

    added vidscola test as exampl number 3

--- test/StepsDefinition.java
+++ test/StepsDefinition.java
@@ -4,7 +4,9 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -89,4 +91,40 @@ public class StepsDefinition {
     }
 
 
+    @When("I select the search icon")
+    public void iSelectTheSearchIcon() {
+        WebDriverWait wait = new WebDriverWait(driver, 20);
+        WebElement searchIcon = wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.className("navbar-search__toggle"))
+        );
+        searchIcon.click();
+    }
+
+    @And("I search for {string}")
+    public void iSearchFor(String courseName) {
+        WebDriverWait wait = new WebDriverWait(driver, 20);
+        WebElement searchBar = wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.name("s"))
+        );
+        searchBar.sendKeys(courseName);
+        searchBar.sendKeys(Keys.ENTER);
+    }
+
+    @And("I select AGILE TESTING AUTOMATION WORKSHOP course")
+    public void iSelectAGILETESTINGAUTOMATIONWORKSHOPCourse() {
+        WebDriverWait wait = new WebDriverWait(driver, 20);
+        WebElement agileTestingAutomationCourse = wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"post-662\"]/div/div[3]/h3/a"))
+        );
+        agileTestingAutomationCourse.click();
+    }
+
+    @Then("I should find {string} as course title")
+    public void iShouldFindAsCourseTitle(String expectedCourseTitle) {
+        WebDriverWait wait = new WebDriverWait(driver, 20);
+        WebElement courseTitle = wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.className("course__title"))
+        );
+        Assert.assertEquals(expectedCourseTitle, courseTitle.getText());
+    }
 }
